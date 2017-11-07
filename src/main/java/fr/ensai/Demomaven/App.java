c2e1d5cf73204d56241a9700307c8f16d35f341b
keita <keita@HP-ZBook-13>
2017-11-07 14:59:08 +0100

    initial commit

---
+++ src/main/java/fr/ensai/Demomaven/App.java
@@ -0,0 +1,17 @@
+package fr.ensai.Demomaven;
+
+/**
+ * Hello world!
+ *Méthode main de l'app
+ @author KEITA Seydu
+ @version 1.0
+ @param args tableau de string
+ @category TP
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
